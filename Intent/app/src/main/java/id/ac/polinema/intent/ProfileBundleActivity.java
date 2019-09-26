ad952de5281c4fd43f3888bad2474b964a11a1f7
oskaaditya <oska.aditya18@gmail.com>
2019-09-26 22:43:53 +0700

    Praktikum Android Bundle

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,8 +3,13 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +17,24 @@ public class ProfileBundleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_bundle);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             // TODO: display value here
+            String value_1;
+            String value_2;
+            int value_3;
+            value_1 = extras.getString("username");
+            value_2 = extras.getString("name");
+            value_3 = extras.getInt("age");
+
+            usernameText.setText(value_1);
+            nameText.setText(value_2);
+            ageText.setText(String.valueOf(value_3));
         }
+
     }
 }
